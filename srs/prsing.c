5797c7c9abcbc899377f3cf88a656f9593103c40
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-05-11 22:55:36 +0100

    a

--- srs/prsing.c
+++ srs/prsing.c
@@ -3,8 +3,13 @@
 void    parsing(char *line)
 {
     char **s;
-    
+    char *tmp;
     s = split(line, '|');
+    
     for(int i = 0; s[i]; i++)
+        // tmp = s[i],
+        // separ_line(s[i]),
+        // free(tmp),
         printf("%s\n", s[i]);
+    
 }
