d885b3d9b639a5c13233a17aa8988faaeba9ce47
Anass Mounadi <amounadi@e1r11p6.1337.ma>
2022-05-18 03:48:54 +0100

    update echo

--- built_in/echo.c
+++ built_in/echo.c
@@ -1,6 +1,6 @@
 #include "../inc/minishell.h"
 
-void print_all(char **cmd)
+/*void print_all(char **cmd)
 {
     int i;
 
@@ -39,4 +39,55 @@ int blt_echo(char **cmd)
     else if (cmd[i] && cmd[i][ft_strlen(cmd[i]) - 1] != '\n' && nl_echo(cmd[0]))
         glob.no_nl = 1;
     return (0);
+}*/
+
+// echo - 
+// echo -  hjkh
+
+int check_and_print(char *str, char **cmd, int a)
+{
+	int i;
+
+	i = 0;
+	if (!a)
+	{
+		if (str && (str[0] != '-' || !str[1]))
+			return(0);
+		while(str && str[++i])
+			if (str[i] != 'n')
+				return (0);
+	}
+	else
+	{
+		while(cmd  && cmd[i])
+		{
+			printf("%s",cmd[i++]);
+			if(cmd[i])
+				printf(" ");
+		}
+		if (a == 1)
+			printf("\n");
+	}
+	return(1);
+}
+
+int blt_echo(char **cmd)
+{
+	int	i;
+	int	nl;
+
+	i = 1;
+	nl = 0;
+	nl = check_and_print(cmd[0], cmd, 0);
+	if(nl && cmd[0])
+	{
+		while(cmd && cmd[i] && nl)
+			nl = check_and_print(cmd[i++], cmd, 0);
+		if (!nl)
+			i--;
+		check_and_print(cmd[0], &cmd[i], 2);
+	}
+	else
+		check_and_print(cmd[0], cmd, 1);
+	return(0);
 }
