32ab0c9adabc56ca405df28e92e33d91acb693f3
Anass Mounadi <amounadi@e1r12p7.1337.ma>
2022-05-25 00:49:07 +0100

    merge

--- parsing/handler_or_and.c
+++ parsing/handler_or_and.c
@@ -66,14 +66,14 @@ char *handler_or_and(char *line)
 /*
 char *or_and(char *line)
 {
-    	int i;
-    	int a;
-    	int check;
+	int i;
+	int a;
+	int check;
 	int lent;
 
-    	a = 0;
-    	i = 0;
-    	check = 1;
+    a = 0;
+    i = 0;
+    check = 1;
 	lent = ft_strlen(line);
 	while(i < lent)
 	{
@@ -81,16 +81,17 @@ char *or_and(char *line)
 		while(line[i])
 		{
 			if(line[i + 1] && ((line[i] == '&' && line[i + 1] == '&') || (line[i] == '|' && line[i + 1] == '|')))
-        	    	{
-                		line[i] = '\0';
+        	{
+         		line[i] = '\0';
 				if ((!glob.exit_code && check == 1) || ((glob.exit_code || a == 0) && check == 2))
-               				mini_cmd((line + a));
+				    mini_cmd((line + a));
 				check = (line[i + 1] == '&') + 2 * (line[i+1] == '|');
                 		a = ++i + 1;
-            			}
-            			i++;
-        		}
-    	}
+            }
+			i++;
+        }
+				
+    }
 	if ((!glob.exit_code && check == 1) || ((glob.exit_code || a == 0) && check == 2))
 		mini_cmd((line + a));
 }
