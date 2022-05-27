772a28f095b0709eff0f4a0707f8b534867decd5
Anass Mounadi <amounadi@e1r11p8.1337.ma>
2022-05-27 15:43:07 +0100

    add

--- parsing/handler_or_and.c
+++ parsing/handler_or_and.c
@@ -65,41 +65,41 @@ char *handler_or_and(char *line)
 	return (line);
 }
 
-// void	or_and(char *line)
-// {
-// 	int i;
-// 	int a;
-// 	int check;
-// 	int lent;
+ void	or_and(char *line)
+ {
+ 	int i;
+ 	int a;
+ 	int check;
+ 	int lent;
 
-// 	a = 0;
-// 	i = 0;
-// 	check = 1;
-// 	if (!line)
-// 		return ;
-// 	lent = ft_strlen(line);
-// 	while (i < lent)
-// 	{
-// 		a = i;
-// 		while (line[i])
-// 		{
-// 			if (line[i + 1] && ((line[i] == '&' && line[i + 1] == '&') || (line[i] == '|' && line[i + 1] == '|')))
-//         	{
-// 				line[i] = '\0';
-// 				if ((!glob.exit_code && check == 1) || (glob.exit_code && check == 2) || !a)
-// 					mini_cmd((line + a));
-// 				check = (line[i + 1] == '&') + 2 * (line[i+1] == '|');
-// 				a = ++i + 1;
-// 			}
-// 			i++;
-//         }
-//     }
-// 	if ((!glob.exit_code && check == 1) || (glob.exit_code && check == 2))
-// 		mini_cmd((line + a));
-// 	else if (!a)
-// 		mini_cmd((line + a));
-// }
-
+ 	a = 0;
+ 	i = 0;
+ 	check = 1;
+ 	if (!line)
+ 		return ;
+ 	lent = ft_strlen(line);
+ 	while (i < lent)
+ 	{
+ 		a = i;
+ 		while (line[i])
+ 		{
+ 			if (line[i + 1] && ((line[i] == '&' && line[i + 1] == '&') || (line[i] == '|' && line[i + 1] == '|')))
+         	{
+ 				line[i] = '\0';
+ 				if ((!glob.exit_code && check == 1) || (glob.exit_code && check == 2) || !a)
+ 					mini_cmd((line + a));
+ 				check = (line[i + 1] == '&') + 2 * (line[i+1] == '|');
+ 				a = ++i + 1;
+ 			}
+ 			i++;
+         }
+     }
+ 	if ((!glob.exit_code && check == 1) || (glob.exit_code && check == 2))
+ 		mini_cmd((line + a));
+ 	else if (!a)
+ 		mini_cmd((line + a));
+ }
+/*
 void	or_and(char *line)
 {
 	int i;
@@ -140,3 +140,4 @@ void	or_and(char *line)
     }
 	free(line);
 }
+*/
